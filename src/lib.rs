5edc0601ecb4f55f120b19eb272a88e2f8188100
Stefan Bühler <source@stbuehler.de>
2021-10-08 17:38:32 +0200

    release 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! provide trees based on bitstrings
 #![warn(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/bitstring-trees/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/bitstring-trees/0.1.1")]
 
 extern crate bitstring;
 
